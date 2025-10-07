102c5ebec3baf8dbf9552dc7577ad6407ff41278
isakalishiri <@isak152528gmail.com>
2025-10-07 09:42:16 +0200

    H2 integrations test push og CI pipeline

--- src/main/java/com/example/touristguide3/repository/TouristAttractionRepository.java
+++ src/main/java/com/example/touristguide3/repository/TouristAttractionRepository.java
@@ -3,23 +3,35 @@ package com.example.touristguide3.repository;
 import com.example.touristguide3.models.City;
 import com.example.touristguide3.models.Tags;
 import com.example.touristguide3.models.TouristAttraction;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class TouristAttractionRepository {
-    private final List<TouristAttraction> attractions = new ArrayList<>();
 
-    public TouristAttractionRepository() {
-        attractions.add(new TouristAttraction( "Tivoli", "Forlystelsespark", City.København, List.of(Tags.Børnevenlig)));
-        attractions.add(new TouristAttraction( "Den Lille Havfrue", "Berømt statue ved havnen", City.København, List.of(Tags.Gratis, Tags.Historie)));
-        attractions.add(new TouristAttraction("Rundetårn", "Høj bygning i Københavns centrum", City.København, List.of(Tags.Historie)));
+    private final JdbcTemplate jdbcTemplate;
+
+    public TouristAttractionRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
+    private final RowMapper<TouristAttraction> rowMapper = (ResultSet rs, int rowNum) -> {
+        TouristAttraction attraction = new TouristAttraction();
+        attraction.setName(rs.getString("name"));
+        attraction.setDescription(rs.getString("description"));
+        attraction.setCity(City.valueOf(rs.getString("city")));
+        attraction.setTags(List.of(Tags.valueOf(rs.getString("tags"))));
+        return attraction;
+    };
+
     public List<TouristAttraction> getAllAttractions() {
-        return attractions;
+        String sql = "SELECT * FROM tourist_attractions";
+        return jdbcTemplate.query(sql, rowMapper);
     }
 
 
@@ -38,6 +50,7 @@ public class TouristAttractionRepository {
         }
         return null;
     }
+
     // Update
     public TouristAttraction updateAttraction(String oldName, String newName, String description, City city, List<Tags> tags) {
         for (TouristAttraction attraction : attractions) {
