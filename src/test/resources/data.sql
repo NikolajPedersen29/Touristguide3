b6a286f6308f8a2f2e274334315b9001ba56dee6
CodeSpitterKEa <olivervinstedi@gmail.com>
2025-10-07 21:33:27 +0200

    refactor repo v2 and cleanup

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,14 @@
-INSERT INTO tourist_attraction (name, description, city, tags)
-VALUES ('Tivoli', 'Forlystelsespark', 'København', 'underholdning,familie');
+-- Test data for attractions
+INSERT INTO tourist_attraction (id, name, description, city)
+VALUES (1, 'Tivoli', 'Forlystelsespark i København', 'COPENHAGEN');
 
-INSERT INTO tourist_attraction (name, description, city, tags)
-VALUES ('Legoland', 'Tema park', 'Billund', 'familie,leg');
+INSERT INTO tourist_attraction (id, name, description, city)
+VALUES (2, 'Legoland', 'Tema park med LEGO', 'BILLUND');
+
+-- Tags for Tivoli
+INSERT INTO attraction_tags (attraction_id, tags) VALUES (1, 'ENTERTAINMENT');
+INSERT INTO attraction_tags (attraction_id, tags) VALUES (1, 'FAMILY');
+
+-- Tags for Legoland
+INSERT INTO attraction_tags (attraction_id, tags) VALUES (2, 'FAMILY');
+INSERT INTO attraction_tags (attraction_id, tags) VALUES (2, 'THEME_PARK');
