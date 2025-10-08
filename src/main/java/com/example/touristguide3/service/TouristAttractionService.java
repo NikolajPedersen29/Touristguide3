b3e3426d149d4979d8f948801cec510704245da7
CodeSpitterKEa <olivervinstedi@gmail.com>
2025-10-08 12:47:26 +0200

    1

--- src/main/java/com/example/touristguide3/service/TouristAttractionService.java
+++ src/main/java/com/example/touristguide3/service/TouristAttractionService.java
@@ -7,7 +7,7 @@ import com.example.touristguide3.repository.TouristAttractionRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+//
 @Service
 public class TouristAttractionService {
 
